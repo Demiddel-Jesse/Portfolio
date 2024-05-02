48fd82d99ca0ba8f934aa255f1a3fb7ee9385b31
Demiddel-Jesse <jesse.demiddel19@gmail.com>
2024-05-02 13:39:23 +0200

    fixed infinite deployment

--- opdracht-5-dashboard-groep-4-goplay-TL4-fixes/gulpfile.js
+++ opdracht-5-dashboard-groep-4-goplay-TL4-fixes/gulpfile.js
@@ -1,23 +1,28 @@
-'use strict';
+"use strict";
 
-const { src, dest, watch, series } = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const gulp = require('gulp');
-const concat = require('gulp-concat');
-const minify = require('gulp-minify');
-const cleancss = require('gulp-clean-css');
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const gulp = require("gulp");
+const concat = require("gulp-concat");
+const minify = require("gulp-minify");
+const cleancss = require("gulp-clean-css");
 
 function buildStyles() {
-	return src('scss/*.scss').pipe(sass()).pipe(cleancss()).pipe(dest('dist'));
+	return src("scss/*.scss").pipe(sass()).pipe(cleancss()).pipe(dest("dist"));
 }
 
 function watchTask() {
-	watch(['scss/**/*.scss'], buildStyles);
-	watch(['js/*.js'], buildJS);
+	watch(["scss/**/*.scss"], buildStyles);
+	watch(["js/*.js"], buildJS);
 }
 
 function buildJS() {
-	return gulp.src(['js/*.js']).pipe(concat('main.js')).pipe(minify()).pipe(gulp.dest('dist'));
+	return gulp
+		.src(["js/*.js"])
+		.pipe(concat("main.js"))
+		.pipe(minify())
+		.pipe(gulp.dest("dist"));
 }
 
 exports.default = series(buildStyles, buildJS, watchTask);
+exports.build = series(buildJS, buildStyles);
